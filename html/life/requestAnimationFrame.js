be1527b60506a00177e7aef13491cb0fd15f224e
Leo Accend <github.aeonic@spamgourmet.com>
2013-01-15 01:17:02 -0800

    Adding requestAnimationFrame polyfill.

---
+++ html/life/requestAnimationFrame.js
@@ -0,0 +1,1 @@
+window.requestAnimationFrame = ( function() { return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function( callback ) { window.setTimeout( callback, 1000 / 60 ); }; } )();
