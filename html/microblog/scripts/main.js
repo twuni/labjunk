e1430a1512a4fba3b563b03efb92ad3ef548b89e
Leo Accend <github.aeonic@spamgourmet.com>
2013-01-19 17:37:27 -0800

    Adding a prototype for a static microblogging platform.

---
+++ html/microblog/scripts/main.js
@@ -0,0 +1,25 @@
+require( [
+  "jquery"
+], function( $ ) {
+
+  var author = decodeURIComponent( location.search.replace( /^.*[&\?]author=([^=&]+)(?:[&\?].*)?$/gi, "$1" ) || "twuni" );
+
+  var $timeline = $("#timeline");
+  var $author = $("<a>").attr( {
+    "rel": "author",
+    "href": "index.html?author=" + encodeURIComponent( author )
+  } ).text( author );
+
+  $.get( "posts/" + author + ".json", function( timeline ) {
+    for( var key in timeline ) {
+      var $post = $("<section>");
+      $post.append( $author.clone() );
+      $post.append( $("<time>").attr( "datetime", key ).text( key ) );
+      $post.append( $("<p>").text( timeline[key] ) );
+      $timeline.append( $post );
+    }
+  } ).error( function() {
+    $timeline.text( "Sorry, " + author + " has no posts." );
+  } );
+
+} );
