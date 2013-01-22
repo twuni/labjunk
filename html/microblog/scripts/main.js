a6116f982e37fdd986024148eec4187defd02980
Leo Accend <github.aeonic@spamgourmet.com>
2013-01-22 13:07:33 -0800

    Adding some basic enhancements to posts, and replacing the header with the author's name.

--- html/microblog/scripts/main.js
+++ html/microblog/scripts/main.js
@@ -6,6 +6,16 @@ require( [
   var authorPattern = /^.*[&\?]author=([^=&]+)(?:[&\?].*)?$/gi;
   var authorName = decodeURIComponent( authorPattern.test(location.search) && location.search.replace( authorPattern, "$1" ) || "twuni" );
 
+  $( "header h1" ).text( authorName );
+
+  var format = function( input ) {
+    var output = input;
+    output = output.replace( /`([^`]+)`/gi, '<code>$1</code>' );
+    output = output.replace( /@([\w]+)/gi, '<a rel="author" href="index.html?author=$1">@$1</a>' );
+    output = output.replace( /#([\w]+)/gi, '<a rel="topic" href="index.html?topic=$1">#$1</a>' );
+    return output;
+  };
+
   var $timeline = $("#timeline");
   var $author = $("<a>").attr( {
     "rel": "author",
@@ -20,6 +30,7 @@ require( [
       src: "http://gravatar.com/avatar/" + author.email + "?s=32&d=identicon",
       title: author.name
     } );
+    $( "header h1" ).text( author.name + " (" + authorName + ")" );
   } );
 
   $.getJSON( "users/" + authorName + "/posts.json", function( timeline ) {
@@ -27,7 +38,9 @@ require( [
       var $post = $("<section>");
       $post.append( $author.clone() );
       $post.append( $("<time>").attr( "datetime", key ).text( prettyDate( key ) ) );
-      $post.append( $("<p>").text( timeline[key] ) );
+      var $body = $("<p>").text( timeline[key] );
+      $body.html( format( $body.text() ) );
+      $post.append( $body );
       $post.hide();
       $timeline.append( $post );
       $post.fadeIn();
